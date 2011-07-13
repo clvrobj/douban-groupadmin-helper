3912c3d4b8b98178d42620ab10a001db3cad1a64
Zhang Chi <clvrobj@gmail.com>
2011-07-13 23:47:34 +0800

    fix bug

--- group_topic.js
+++ group_topic.js
@@ -2,7 +2,7 @@ var lnks = $('.topic-opt a'),
 ban = $(".topic-opt a:contains('封禁')"),
 del = $(".topic-opt a:contains('删除')");
 
-if (ban && del) {
+if (ban[0] && del[0]) {
     var ban_del = $('<a href="#">&gt;封禁并删除</a>').click(
         function () {
             $.ajax({url: ban.attr('href'),
